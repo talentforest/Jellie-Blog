70cbc1a7a02fa1a39dd87a053f728bf9e465da4f
jellie <talentforest0501@gmail.com>
2023-03-26 03:01:52 +0900

    feat: progress bar by scroll height

--- components/posts/post-contents.tsx
+++ components/posts/post-contents.tsx
@@ -1,4 +1,5 @@
 import MarkdownViewer from './markdown-viewer';
+import ProgressBar from './progress-bar';
 
 interface Props {
   contents: {
@@ -13,11 +14,12 @@ export default function PostContents({
   contents: { category, title, date, content },
 }: Props) {
   return (
-    <section className='relative p-4 py-6 mt-60 bg-slate-100'>
-      <h2 className='py-1 px-3 mb-4 rounded-full bg-indigo-500 text-white text-xs w-fit'>
+    <section className='relative px-4 pb-6 mt-60 bg-slate-100'>
+      <ProgressBar />
+      <h2 className='py-1 px-3 mb-4 mt-8 rounded-full bg-indigo-500 text-white text-xs w-fit'>
         {category}
       </h2>
-      <h3 className='font-sans absolute top-8 right-4 text-slate-600 text-sm'>
+      <h3 className='font-sans absolute top-14 right-4 text-slate-600 text-sm'>
         {new Date(date).toLocaleDateString()}
       </h3>
       <h1 className='text-2xl font-bold'>{title}</h1>
---
+++ components/posts/progress-bar.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { useCallback, useEffect, useState } from 'react';
+
+export default function ProgressBar() {
+  const [width, setWidth] = useState(0);
+
+  const handleScroll = useCallback((): void => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    if (scrollTop === 0) return setWidth(0);
+    const windowHeight: number = scrollHeight - clientHeight;
+    const currentPercent: number = scrollTop / windowHeight;
+    setWidth(currentPercent * 100);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, true);
+    return () => {
+      window.removeEventListener('scroll', handleScroll, true);
+    };
+  }, [handleScroll]);
+
+  return (
+    <div className='sticky top-14 md:top-16 border h-4 -mx-4 rounded-md z-10 w-screen bg-indigo-100'>
+      <div
+        style={{ width: width + '%' }}
+        className='h-full rounded-md px-1 bg-indigo-500 text-[10px] text-white'
+      >
+        {width.toFixed()}%
+      </div>
+    </div>
+  );
+}
