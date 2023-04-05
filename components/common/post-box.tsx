8e9f4f3cbf573e08990db1d9c4f85d7e80e5acde
jellie <talentforest0501@gmail.com>
2023-04-05 23:48:55 +0900

    refactoring: post-box size props

--- components/common/post-box.tsx
+++ components/common/post-box.tsx
@@ -4,26 +4,26 @@ import Link from 'next/link';
 
 interface PostBoxProps {
   post: Post;
-  size?: 'sm' | 'lg';
   prev?: boolean;
+  sm?: boolean;
 }
 
-export default function PostBox({ post, size = 'sm', prev }: PostBoxProps) {
+export default function PostBox({ post, prev, sm = false }: PostBoxProps) {
   const { path, category, title, description } = post;
 
   return (
     <Link
       key={path}
       href={`/posts/${path}`}
-      className={`${
-        size === 'sm' ? 'items-start' : 'items-center'
-      } flex justify-between w-full h-full bg-box group hover:-translate-y-0.5 border border-slate hover:border-2 hover:border-yellow transition cursor-pointer rounded-xl p-2.5 select-none`}
+      className='flex justify-between items-center w-full h-full bg-box group hover:-translate-y-0.5 border border-slate hover:border-2 hover:border-yellow transition cursor-pointer rounded-xl p-2.5 select-none'
     >
-      {size !== 'sm' && prev && (
+      {!sm && prev && (
         <HiArrowLeft className='w-8 h-8 text-text group-hover:text-yellow' />
       )}
       <article
-        className={`${prev ? 'items-end' : 'items-start'} w-full flex flex-col`}
+        className={`${
+          prev ? 'items-end' : 'items-start'
+        } w-full flex flex-col h-full`}
       >
         <h4 className='text-xs text-white mb-2 font-extralight border border-slate px-2 py-0.5 bg-indigo rounded-md w-fit'>
           {category}
@@ -31,7 +31,7 @@ export default function PostBox({ post, size = 'sm', prev }: PostBoxProps) {
         <h2 className='mb-1 font-bold text-lg'>{title}</h2>
         <p className='text-sm mb-2 text-slate'>{description}</p>
       </article>
-      {size !== 'sm' && !prev && (
+      {!sm && !prev && (
         <HiArrowRight className='w-8 h-8 text-text group-hover:text-yellow' />
       )}
     </Link>
