a27aa9de30daba23a1b78d1a35f1c291b6b98fdf
jellie <talentforest0501@gmail.com>
2023-04-12 18:59:50 +0900

    fix: remove useEffect dependency array in progress bar

--- components/posts/progress-bar.tsx
+++ components/posts/progress-bar.tsx
@@ -9,17 +9,17 @@ export default function ProgressBar() {
   const handleScroll = throttle((): void => {
     const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
     if (scrollTop === 0) return setWidth(0);
-    const windowHeight: number = scrollHeight - clientHeight;
-    const currentPercent: number = scrollTop / windowHeight;
+    const windowHeight = scrollHeight - clientHeight;
+    const currentPercent = scrollTop / windowHeight;
     setWidth(currentPercent * 100);
-  }, 100);
+  }, 200);
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, [handleScroll]);
+  }, []);
 
   return (
     <div className='sticky w-full top-12 md:top-14 h-3 z-10 bg-slate border-slate'>
