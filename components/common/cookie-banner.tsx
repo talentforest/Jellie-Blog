4f409a3bec2eb9390b36fcff68e3710e32145343
jellie <talentforest0501@gmail.com>
2023-04-22 02:30:22 +0900

    fix: window.gtag

--- components/common/cookie-banner.tsx
+++ components/common/cookie-banner.tsx
@@ -14,9 +14,11 @@ export default function CookieBanner() {
 
   useEffect(() => {
     const newValue = cookieConsent ? 'granted' : 'denied';
-    window.gtag('consent', 'update', {
-      analytics_storage: newValue,
-    });
+    if (typeof window !== 'undefined') {
+      window.gtag('consent', 'update', {
+        analytics_storage: newValue,
+      });
+    }
     setLocalStorage('cookie_consent', cookieConsent);
   }, [cookieConsent]);
 
