c38180ede54e302fb0690658f171bd8a7c51db76
jellie <talentforest0501@gmail.com>
2023-04-24 04:41:33 +0900

    remove image config in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/photo*',
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
